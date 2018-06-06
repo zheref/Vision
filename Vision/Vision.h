66555714afef01dd68bce5f11a2bc812f3e87cc2
Sergio Daniel Lozano <sergio.x.lozano.-nd@disney.com>
2018-06-06 12:00:21 -0500

    Initial Commit

---
+++ Vision/Vision.h
@@ -0,0 +1,19 @@
+//
+//  Vision.h
+//  Vision
+//
+//  Created by Sergio Lozano García on 6/6/18.
+//  Copyright © 2018 Sergio Lozano García. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vision.
+FOUNDATION_EXPORT double VisionVersionNumber;
+
+//! Project version string for Vision.
+FOUNDATION_EXPORT const unsigned char VisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vision/PublicHeader.h>
+
+
